2ca9217d7a545fc8debf597a230b77ccd05369cd
GabrielNeeeves <gabrielnevesferreira08@gmail.com>
2023-12-05 14:08:44 -0300

    perf inc

--- gerenciador_de_tarefas/src/main/java/com/example/Main.java
+++ gerenciador_de_tarefas/src/main/java/com/example/Main.java
@@ -1,7 +1,43 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        Tarefa novaTarefa = null;
+        
+        System.out.println("Digite seu primeiro nome: ");
+        String username = scanner.next();
+
+        System.out.println("Olá "+username+", o que deseja?");
+        
+        boolean sair = false;
+        while(!sair) {
+            System.out.println("# para cada ação:");
+            System.out.println("1. Criar nova tarefa");
+            System.out.println("2. Ver Tarefas");
+            System.out.println("3. Sair");
+            int opcao = scanner.nextInt();
+
+            switch(opcao) {
+                case 1:
+                    novaTarefa = Tarefa.criarTarefa(scanner);
+                    break;
+                case 2:
+                    System.out.println(novaTarefa.getNome());
+                    break;
+                case 3:
+                    sair = true;
+                    break;
+                default:
+                    System.out.println("Tente novamente.");
+                    break;
+            }
+        }
+        System.out.println(username+" saiu.");
+        scanner.close();
     }
 }
--- gerenciador_de_tarefas/src/main/java/com/example/Tarefa.java
+++ gerenciador_de_tarefas/src/main/java/com/example/Tarefa.java
@@ -1,10 +1,19 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Tarefa {
     private String nome;
     private String descricao;
     private int prioridade;
 
+    //CONSTRUTOR
+    public Tarefa(String n, String desc, int prio) {
+        this.nome = n;
+        this.descricao = desc;
+        this.prioridade = prio;
+    }
+    
     //NOME
     public String getNome() {
         return nome;
@@ -30,6 +39,26 @@ public class Tarefa {
 
     public void setPrioridade(int prioridade) {
         this.prioridade = prioridade;
+    }
+
+    //MÉTODO CRIAR TAREFA
+    public static Tarefa criarTarefa(Scanner scanner) {
+
+        System.out.println("Detalhes da tarefa:");
+        System.out.println("Nome: ");
+        String nome = scanner.next();
+
+        scanner.nextLine();
+
+        System.out.println("Descrição: ");
+        String desc = scanner.next();
+
+        scanner.nextLine();
+        
+        System.out.println("Prioridade: ");
+        int prio = scanner.nextInt();
+
+        return new Tarefa(nome, desc, prio);
     }
 
 }
